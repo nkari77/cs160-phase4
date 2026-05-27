ad1a9f6cf5e0e5e35d881f6d561624808e49b7da
nkari77 <nkari011@ucr.edu>
2026-05-27 04:00:26 +0000

    updated graph.h with query request types and versioned edges

--- code/graph.h
+++ code/graph.h
@@ -1,29 +1,36 @@
 #pragma once
 #include <cstdint>
 #include <vector>
-#include <mutex>
+#include <shared_mutex>
 
 using namespace std;
 
 struct Edge {
     uint32_t dst;
     int      weight;
+    uint64_t version;
 };
 
-struct AddRequest {
+struct UpdateReq {
     uint32_t src;
     uint32_t dst;
     int      weight;
+    uint64_t version;
+};
+
+struct QueryReq {
+    uint64_t         version;
+    vector<uint32_t> dirty;
 };
 
 struct Graph {
-    vector<vector<Edge>> adj;
-    vector<mutex>        locks;
+    vector<vector<Edge>>  adj;
+    vector<shared_mutex>  locks;
 
     explicit Graph(uint32_t n) : adj(n), locks(n) {}
 
-    void apply(const AddRequest& r) {
-        lock_guard<mutex> lk(locks[r.src]);
-        adj[r.src].push_back({r.dst, r.weight});
+    void apply(const UpdateReq& r) {
+        unique_lock<shared_mutex> lk(locks[r.src]);
+        adj[r.src].push_back({r.dst, r.weight, r.version});
     }
 };
