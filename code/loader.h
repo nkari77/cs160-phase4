08a7bfaac29f868abdabdb98f14955c2ea8d48fb
nkari77 <nkari011@ucr.edu>
2026-05-27 04:35:07 +0000

    updated loader.h

--- code/loader.h
+++ code/loader.h
@@ -4,7 +4,7 @@
 #include <sstream>
 #include <string>
 #include <tuple>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -27,27 +27,43 @@ Graph load_initial(const string& path) {
 
     Graph g(maxv + 1);
     for (int i = 0; i < (int)edges.size(); i++)
-        g.adj[get<0>(edges[i])].push_back({get<1>(edges[i]), get<2>(edges[i])});
+        g.adj[get<0>(edges[i])].push_back({get<1>(edges[i]), get<2>(edges[i]), 0});
     return g;
 }
 
-void parse_updates(const string& path,
-                   vector<AddRequest>& adds,
-                   vector<Delay>& delays)
-{
+struct ParsedTrace {
+    vector<UpdateReq> updates;
+    vector<QueryReq>  queries;
+    vector<Delay>     delays;
+};
+
+ParsedTrace parse_updates(const string& path, uint32_t source) {
     ifstream f(path);
     string line;
+
+    ParsedTrace out;
+    uint64_t ver = 0;
+    vector<uint32_t> pending_dirty = {source};
+
     while (getline(f, line)) {
         if (line.empty()) continue;
         istringstream ss(line);
         string tag; ss >> tag;
+
         if (tag == "ADD") {
             uint32_t s, d; int wt;
             ss >> s >> d >> wt;
-            adds.push_back({s, d, wt});
+            ++ver;
+            out.updates.push_back({s, d, wt, ver});
+            pending_dirty.push_back(s);
+        } else if (tag == "Q") {
+            ++ver;
+            out.queries.push_back({ver, pending_dirty});
+            pending_dirty.clear();
         } else if (tag == "D") {
             int ms; ss >> ms;
-            delays.push_back({adds.size(), ms});
+            out.delays.push_back({out.updates.size(), ms});
         }
     }
+    return out;
 }
